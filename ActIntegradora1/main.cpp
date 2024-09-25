eda6f0dd53d754640552d28d9bd9b43d9c185a3a
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2024-09-25 15:27:49 -0600

    Upsate: Solution

--- ActIntegradora1/main.cpp
+++ ActIntegradora1/main.cpp
@@ -7,14 +7,7 @@ vector <string> vecTrans1;
 ifstream transmission1;
 transmission1.open("transmission1.txt"); //open file
 
-for(string listOfWords; getline(transmission1, listOfWords, '\n'); ){
-    vecTrans1.push_back(listOfWords);
-} 
-
-for (int i = 0; i < vecTrans1.size(); i++){
-    cout << vecTrans1[i]<< endl;
-}
-
+textToVec (transmission1, vecTrans1);
 
 
 return 0;
--- ActIntegradora1/solution.h
+++ ActIntegradora1/solution.h
@@ -5,3 +5,13 @@
 
 using namespace std;
 
+void textToVec (ifstream& text, vector <string>& vec){
+    
+    for(string line; getline(text, line, '\n'); ){
+        vec.push_back(line);
+    } 
+
+    for (int i = 0; i < vec.size(); i++){
+        cout << vec[i] << endl;
+    }
+};
