f6bffdc0aba482ac38bb328b6e8dce1694d0bd09
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2024-09-25 17:01:29 -0600

    Update: Avance

--- ActIntegradora1/main.cpp
+++ ActIntegradora1/main.cpp
@@ -4,48 +4,22 @@ int main()
 {
 
 vector <string> vecTrans1;
-ifstream transmission1;
-transmission1.open("transmission1.txt"); //open file
-
 vector <string> vecTrans2;
-ifstream transmission2;
-transmission2.open("transmission2.txt"); //open file
-
 vector <string> vecMcode1;
-ifstream mcode1;
-mcode1.open("mcode1.txt"); //open file
-
 vector <string> vecMcode2;
-ifstream mcode2;
-mcode2.open("mcode2.txt"); //open file
-
 vector <string> vecMcode3;
+
+ifstream transmission1;
+ifstream transmission2;
+ifstream mcode1;
+ifstream mcode2;
 ifstream mcode3;
+
+transmission1.open("transmission1.txt"); //open file
+transmission2.open("transmission2.txt"); //open file
+mcode1.open("mcode1.txt"); //open file
+mcode2.open("mcode2.txt"); //open file
 mcode3.open("mcode3.txt"); //open file
-
-
-
-textToVec (transmission1, vecTrans1);
-
-cout << "\n"<< endl;
-
-textToVec (transmission2, vecTrans2);
-
-cout << "\n"<< endl;
-
-
-
-textToVec (mcode1, vecMcode1);
-
-cout << "\n"<< endl;
-
-textToVec (mcode2, vecMcode2);
-
-cout << "\n"<< endl;
-
-textToVec (mcode3, vecMcode3);
-
-cout << "\n"<< endl;
 
 
 return 0;
