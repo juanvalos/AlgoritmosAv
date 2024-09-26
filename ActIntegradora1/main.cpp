e28cea76a189c916eedb72e00d35b438f59fd967
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2024-09-26 01:28:36 -0600

    Update: Solution Part 1

--- ActIntegradora1/main.cpp
+++ ActIntegradora1/main.cpp
@@ -5,9 +5,9 @@ int main()
 
 vector <string> vecTrans1;
 vector <string> vecTrans2;
-vector <string> vecMcode1;
-vector <string> vecMcode2;
-vector <string> vecMcode3;
+string strMcode1;
+string strMcode2;
+string strMcode3;
 
 ifstream transmission1;
 ifstream transmission2;
@@ -21,6 +21,33 @@ mcode1.open("mcode1.txt"); //open file
 mcode2.open("mcode2.txt"); //open file
 mcode3.open("mcode3.txt"); //open file
 
+txtToVec(transmission1, vecTrans1);
+txtToVec(transmission2, vecTrans2);
+txtToString(mcode1, strMcode1);
+txtToString(mcode2, strMcode2);
+txtToString(mcode3, strMcode3);
+
+cout << "\n" << endl;
+cout << "-------- Find malicious code in each transmission --------" << endl;
+cout << "\n" << endl;
+
+cout << "          Transmission 1          " << endl;
+malCodeTester(vecTrans1, strMcode1, strMcode2, strMcode3);
+cout << "\n" << endl;
+cout << "          Transmission 2          " << endl;
+malCodeTester(vecTrans2, strMcode1, strMcode2, strMcode3);
+
+
+
+cout << "\n" << endl;
+cout << "-------- Find palindormes in each transmision --------" << endl;
+cout << "\n" << endl;
+
+cout << "          Transmission 1          " << endl;
+malCodeTester(vecTrans1, strMcode1, strMcode2, strMcode3);
+cout << "\n" << endl;
+cout << "          Transmission 2          " << endl;
+malCodeTester(vecTrans2, strMcode1, strMcode2, strMcode3);
 
 return 0;
 }
--- ActIntegradora1/solution.h
+++ ActIntegradora1/solution.h
@@ -5,14 +5,85 @@
 
 using namespace std;
 
-void textToVec (ifstream& text, vector <string>& vec){
-    
-    for(string line; getline(text, line, '\n'); ){
-        vec.push_back(line);
-    } 
+// Converts a .txt file into a vector, in each vector index is a line of the .txt file
+void txtToVec (ifstream& txt, vector <string>& vec){
+    for(string str; getline(txt, str, '\n'); ){
+        vec.push_back(str);
+    }
+};
+
+// Converts a .txt file into a string
+void txtToString (ifstream& txt, string& str) {
+    getline(txt, str);
+};
+
+// Checks if a substring is in a certain string
+bool mcodeExists (string str, string substr) {
+    return str.find(substr) != string::npos;
+};
+
+bool palindromeExists(string str){
 
 };
 
-bool mcodeExists (string str, string substr) {
-    return str.find(substr) != string::npos;
+// Shows in terminal where a substring starts and finishes on a certain string
+void stringCount (string& str, string& substr){
+    size_t inicio = str.find(substr);
+    size_t fin = inicio + substr.length() - 1;
+    cout << "  chars from "<< inicio << " to " << fin << "." << endl;
+};
+
+// Find malicious code in a transmission (Part1)
+void malCodeTester(vector <string>& vec, string& malcode1, string& malcode2, string& malcode3) {
+    string transm;
+    string malcode;
+    int flags[3] = {0,0,0};
+    int line = 1;
+
+    for (int i = 0; i < vec.size(); i++) {
+        transm = vec[i];
+        
+        if (mcodeExists(transm, malcode1) == 1){
+            malcode = malcode1;
+            cout << "True  " << malcode << "  line: " << line + i;
+            stringCount(transm, malcode);
+            flags[0] = 1;
+        }
+        
+
+        if (mcodeExists(transm, malcode2) == 1){
+            malcode = malcode2;
+            cout << "True  " << malcode << "  line: " << line + i;
+            stringCount(transm, malcode);
+            flags[1] = 1;
+        }
+
+        if (mcodeExists(transm, malcode3) == 1){
+            malcode = malcode3;
+            cout << "True  " << malcode << "  line: " << line + i;
+            stringCount(transm, malcode);
+            flags[2] = 1;
+        }
+    }
+
+    if (flags[0] == 0){
+        cout << "False  " << malcode1 << endl;
+    }
+
+    if (flags[1] == 0){
+        cout << "False  " << malcode2 << endl;
+    }
+
+    if (flags[2] == 0){
+        cout << "False  " << malcode3 << endl;
+    }
+};
+
+void findPalindomesTester (vector <string>& vec){
+    string transm;
+    int flag = 0;
+
+    for (int i = 0; i < vec.size(); i++){
+
+    }
 };
